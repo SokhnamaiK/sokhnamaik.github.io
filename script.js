b879ff730c35fd89189519f09970e8c881bf3169
Sokhnamai Kane <sokhnamai@Sokhnamais-MacBook-Air.local>
2021-05-27 10:49:47 -0400

    Changes to Website

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-fancub dflnsdl
+
+<script>
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
+
+</body>
+</html>
